82e60836bc90e9b20580557d094670b9f72cb21a
Heedharyhart <idayatolaide0@gmail.com>
2022-03-16 15:26:21 -0700

    Print 24 Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * jack_bauer - print every minute of the day of jack bauer
  * starting from 00:00 to 23:59
@@ -12,8 +11,7 @@ while (i < 24)
 while (j < 60)
 {_putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
-_putchar(':');
-_putchar((j / 10) + '0'); 
+_putchar(':'); _putchar((j / 10) + '0'); 
 _putchar((j % 10) + '0');
 _putchar('\n');
 j++; }
