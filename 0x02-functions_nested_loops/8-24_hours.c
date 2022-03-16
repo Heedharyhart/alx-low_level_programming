3923b34d3451a35d020e82241198b40e3c44e3fb
Heedharyhart <idayatolaide0@gmail.com>
2022-03-16 15:48:19 -0700

    Times Table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,8 @@ while (i < 24)
 while (j < 60)
 {_putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
-_putchar(':'); _putchar((j / 10) + '0'); 
+_putchar(':');
+_putchar((j / 10) + '0'); 
 _putchar((j % 10) + '0');
 _putchar('\n');
 j++; }
