8123c5747aac49a4c9f0053006a2bd66d811337d
Heedharyhart <idayatolaide0@gmail.com>
2022-03-16 18:09:26 -0700

    Time table 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {k = j * i;
 if (j == 0)
-{_putchar(k + '0'); }i 
+{_putchar(k + '0'); }
 else if (k < 10 && j != 0)
 {_putchar(',');
 _putchar(' ');
