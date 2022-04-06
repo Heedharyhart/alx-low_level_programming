ba9195a4977a46d945aa0c3ecd375c5f4ed511af
Idayat Abdulkareem <idayatabdulkareem@Administrators-MacBook-Pro-7.local>
2022-04-07 00:22:33 +0100

    Static Library

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * _memset - fill memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: byte filled.
- * Return: the pointer to dest.
- */
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		*(s + i) = b;
-
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
 	return (s);
 }
