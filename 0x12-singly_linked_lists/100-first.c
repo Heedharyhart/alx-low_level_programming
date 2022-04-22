ecde906dace0b71c4569def305d425b4dbc040e0
Idayat Abdulkareem <idayatabdulkareem@Administrators-MacBook-Pro-7.local>
2022-04-23 00:02:37 +0100

    added sinle link

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * bmain - function executed before main
- * Return: Always 0
-*/
-
-void bmain(void) __attribute__ ((constructor));
-
-void bmain(void)
+ * first - print a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-
-int main(void)
-{
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
 }
